c2daaebca3a7161d4c9b179468314d86e45bdc0f
Edgar <zorro.ej@gmail.com>
2018-08-14 11:43:25 +0800

    seeding

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -3,5 +3,5 @@ class Rental < ApplicationRecord
   has_many :bookings
   has_many :reviews
 
-  validates :user, :rental_type, :accomodates, :bathrooms, :bedrooms, :beds, :nightly_cost, :street1, :city, :country, presence: true
+  validates :user, :rental_type, :accommodates, :bathrooms, :bedrooms, :beds, :nightly_cost, :street1, :city, :country, presence: true
 end
